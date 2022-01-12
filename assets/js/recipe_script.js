20ee4c37c9e64f972cf379d4fdfd2c1c4a476d7c
Rochelle Younger <regnuoy321@gmail.com>
2022-01-11 21:13:58 -0500

    loop fix

--- assets/js/recipe_script.js
+++ assets/js/recipe_script.js
@@ -23,76 +23,87 @@ var test = function (event) {
     console.log(inputEl);
 
     function fetchRecipe (input) {
-        var requestUrl = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=db254b5cd61744d39a2deebd9c361444&ingredients=" + input + "&number=1";
+        var requestUrl = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=db254b5cd61744d39a2deebd9c361444&ingredients=" + input + "&number=9";
       
         fetch(requestUrl).then(function(response) {
             if (response.ok) {
                 response.json().then(function(data) {
-                    console.log(data);
-                    console.log(data[0].title)
-                    var divEl = document.createElement("section");
-                    divEl.className = "recipe-section";
-                    var recipeNameEl = document.createElement("h2");
-                    recipeNameEl.innerText = data[0].title;
-                    divEl.appendChild(recipeNameEl);
+                    console.log(data[i]);
+                    for (var i=0; i<data.length; i++) {
+                        var recipeSectionEl = document.createElement("section");
+                        recipeSectionEl.className = "recipe-section";
+    
+                        var recipeId = data[i].id;
+                        var requestUrl = "https://api.spoonacular.com/recipes/" + recipeId +"/information?apiKey=948e50c68fc14d59b9b4ff776f8fa614&includeNutrition=false"
+    
+                        function getInfo() {
+                            fetch(requestUrl).then(function(response) {
+                                if (response.ok) {
+                                    response.json().then(function(data) {
+                                        console.log(data);
+                                        console.log(data.analyzedInstructions);
+                                        console.log(data.analyzedInstructions[0].steps);
 
-                    var recipeImg = document.createElement("img");
-                    recipeImg.src=data[0].image;
-                    divEl.appendChild(recipeImg);
+                                        var recipeCardEl = document.createElement("div");
+                                        recipeCardEl.classList.add("recipe-card");
+                                        recipeSectionEl.appendChild(recipeCardEl);
 
-                    var recipeId = data[0].id;
-                    var requestUrl = "https://api.spoonacular.com/recipes/" + recipeId +"/information?apiKey=948e50c68fc14d59b9b4ff776f8fa614&includeNutrition=false"
+                                        var recipeNameEl = document.createElement("h1");
+                                        recipeNameEl.innerText = data.title;
+                                        recipeCardEl.appendChild(recipeNameEl);
 
-                    function getInfo() {
-                        fetch(requestUrl).then(function(response) {
-                            if (response.ok) {
-                                response.json().then(function(data) {
-                                    console.log(data);
-                                    var recipeIngredientsEl = document.createElement("h2")
-                                    recipeIngredientsEl.innerText = "Ingredients"
-                                    divEl.appendChild(recipeIngredientsEl);
-
-                                    for (var i=0; i < data.extendedIngredients.length; i++) {
-                                        var ingredientEl = document.createElement("p");
-                                        ingredientEl.innerText = data.extendedIngredients[i].name;
-                                        divEl.appendChild(ingredientEl);
-                                    }
-                                    var recipeStepsEl = document.createElement("h2");
-                                    recipeStepsEl.innerText = "Instructions";
-                                    divEl.appendChild(recipeStepsEl);
-
-                                    for (var i=0; i < data.analyzedInstructions[0].steps.length; i++) {
-                                        var stepEl = document.createElement("p");
-                                        stepEl.innerText = "Step " + (i+1) + "   " +  data.analyzedInstructions[0].steps[i].step;
-                                        divEl.appendChild(stepEl);
-                                    }
-
-                                    var linkEl = document.createElement("h2");
-                                    linkEl.innerText = "Link";
-                                    divEl.appendChild(linkEl);
-
-                                    var text = document.createElement("p");
-                                    text.innerText = "View recipe website ";
-                                    divEl.appendChild(text);
-
-                                    var span = document.createElement("span");
-                                    text.appendChild(span);
-
-                                    var link = document.createElement("a");
-                                    link.innerText = "here";
-                                    link.setAttribute("href", data.sourceUrl)
-                                    link.setAttribute("target", "_blank");
-                                    span.appendChild(link);
-
-                                    
-                                })
-                            }
-                        })
+                                        
+                                        var recipeImg = document.createElement("img");
+                                        recipeImg.src = data.image;
+                                        recipeCardEl.appendChild(recipeImg);
+                    
+                                        var recipeIngredientsEl = document.createElement("h2")
+                                        recipeIngredientsEl.innerText = "Ingredients"
+                                        recipeCardEl.appendChild(recipeIngredientsEl);
+    
+                                        for (var i=0; i < data.extendedIngredients.length; i++) {
+                                            var ingredientEl = document.createElement("p");
+                                            ingredientEl.innerText = data.extendedIngredients[i].name;
+                                            recipeCardEl.appendChild(ingredientEl);
+                                        }
+                                        var recipeStepsEl = document.createElement("h2");
+                                        recipeStepsEl.innerText = "Instructions";
+                                        recipeCardEl.appendChild(recipeStepsEl);
+    
+                                        for (var i=0; i < data.analyzedInstructions[0].steps.length; i++) {
+                                            var stepEl = document.createElement("p");
+                                            stepEl.innerText = "Step " + (i+1) + "   " +  data.analyzedInstructions[0].steps[i].step;
+                                            recipeCardEl.appendChild(stepEl);
+                                        }
+    
+                                        var linkEl = document.createElement("h2");
+                                        linkEl.innerText = "Link";
+                                        recipeCardEl.appendChild(linkEl);
+    
+                                        var text = document.createElement("p");
+                                        text.innerText = "View recipe website ";
+                                        recipeCardEl.appendChild(text);
+    
+                                        var span = document.createElement("span");
+                                        recipeCardEl.appendChild(span);
+    
+                                        var link = document.createElement("a");
+                                        link.innerText = "here";
+                                        link.setAttribute("href", data.sourceUrl)
+                                        link.setAttribute("target", "_blank");
+                                        span.appendChild(link);
+    
+                                        
+                                    })
+                                }
+                            })
+                        }
+    
+                        getInfo();
+    
+                        document.querySelector("body").appendChild(recipeSectionEl);
                     }
-
-                    getInfo();
-
-                    document.querySelector("body").appendChild(divEl);
+                   
 
                 })
         
